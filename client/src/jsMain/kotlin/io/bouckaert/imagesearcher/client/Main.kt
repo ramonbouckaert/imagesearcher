fcff2511a01e4f9fd455d47f43d3ce866398c1f3
Ramon Bouckaert <ramon@bouckaert.com.au>
2026-04-20 00:25:09 +1000

    Further iteration on grid styling

--- client/src/jsMain/kotlin/io/bouckaert/imagesearcher/client/Main.kt
+++ client/src/jsMain/kotlin/io/bouckaert/imagesearcher/client/Main.kt
@@ -405,8 +405,9 @@ private suspend fun loadPage(grid: HTMLDivElement, status: HTMLParagraphElement,
         img.setAttribute("decoding", "async")
         img.addEventListener("load", {
             val ratio = (img.naturalWidth.toDouble() / img.naturalHeight).coerceIn(0.5, 2.5)
-            link.style.setProperty("flex-grow", (ratio * 2).toString())
-            link.style.setProperty("flex-basis", "${(ratio * 220).toInt()}px")
+            link.style.setProperty("flex-grow", ratio.coerceAtLeast(1.0).toString())
+            link.style.setProperty("aspect-ratio", ratio.toString())
+            link.style.setProperty("flex-basis", "${(ratio * 15).toInt()}vmax")
             if (ratio < 1.5) link.style.setProperty("max-width", "calc(50% - 0.25rem)")
             else link.style.setProperty("max-width", "none")
             link.style.opacity = "1"
