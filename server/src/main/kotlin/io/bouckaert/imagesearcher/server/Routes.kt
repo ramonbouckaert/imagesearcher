f9fc3bcc12a62c2a6d3f8545f14bc8eb53dc4086
Ramon Bouckaert <ramon@bouckaert.com.au>
2026-05-26 21:38:04 +1000

    Better headers for cache control

--- server/src/main/kotlin/io/bouckaert/imagesearcher/server/Routes.kt
+++ server/src/main/kotlin/io/bouckaert/imagesearcher/server/Routes.kt
@@ -1,10 +1,13 @@
 package io.bouckaert.imagesearcher.server
 
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.http.content.*
+import io.ktor.server.request.header
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.notExists
@@ -34,6 +37,12 @@ fun Routing.searchRoutes(index: LuceneIndex, basePath: String, libraryRoot: Path
         if (resolvedPath.notExists() || !resolvedPath.isRegularFile()) {
             return@get call.respond(HttpStatusCode.NotFound)
         }
+        val eTag = "\"${Files.getLastModifiedTime(resolvedPath).toMillis()}\""
+        call.response.header(HttpHeaders.ETag, eTag)
+        call.response.header(HttpHeaders.CacheControl, "max-age=86400")
+        if (call.request.header(HttpHeaders.IfNoneMatch) == eTag) {
+            return@get call.respond(HttpStatusCode.NotModified)
+        }
         val thumbnail = thumbnailCache.get(resolvedPath)
             ?: return@get call.respond(HttpStatusCode.InternalServerError)
         call.respondBytes(thumbnail, ContentType.parse("image/avif"))
@@ -47,6 +56,12 @@ fun Routing.searchRoutes(index: LuceneIndex, basePath: String, libraryRoot: Path
         }
         if (resolvedPath.notExists() || !resolvedPath.isRegularFile()) {
             return@get call.respond(HttpStatusCode.NotFound)
+        }
+        val eTag = "\"${Files.getLastModifiedTime(resolvedPath).toMillis()}\""
+        call.response.header(HttpHeaders.ETag, eTag)
+        call.response.header(HttpHeaders.CacheControl, "max-age=86400")
+        if (call.request.header(HttpHeaders.IfNoneMatch) == eTag) {
+            return@get call.respond(HttpStatusCode.NotModified)
         }
         val contentType = when (resolvedPath.toFile().extension.lowercase()) {
             "jpg", "jpeg" -> ContentType.Image.JPEG
